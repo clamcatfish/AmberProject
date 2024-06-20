00a70bf9163b795155180247b433a8afa3a827ad
calum <clam0540@gmail.com>
2024-06-20 13:00:02 -0600

    commented out line in prisma.js

--- app/libs/prisma.js
+++ app/libs/prisma.js
@@ -2,6 +2,6 @@ import { PrismaClient } from "@prisma/client"
 
 const client = globalThis.PrismaClient || new PrismaClient()
 
-if(process.env.NODE_ENV !== "production") globalThis.prisma = client;
+// if(process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
 export default client;
